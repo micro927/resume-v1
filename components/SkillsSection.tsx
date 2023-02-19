7f0c71e8673b9485cc6325d81474ab82912be61a
micro927 <nsbelieve-15@hotmail.com>
2023-02-20 00:17:28 +0700

    update: remove card bg on lg

--- components/SkillsSection.tsx
+++ components/SkillsSection.tsx
@@ -26,11 +26,11 @@ function SkillsSection({ techSkills }: Props) {
     return (
         <section id='skills' className='flex flex-col w-full min-h-max scroll-mt-8 md:scroll-mt-16 mb-12'>
             <h6 className='section-header'>Skills</h6>
-            <div className="flex flex-row flex-wrap w-full justify-start items-start lg:justify-between p-8 lg:p-16 bg-slate-500 bg-opacity-10 rounded-2xl my-6">
+            <div className="flex flex-row flex-wrap w-full justify-start items-start lg:justify-between p-8 lg:p-16 bg-slate-500 bg-opacity-10 lg:bg-opacity-0 rounded-2xl my-6">
                 {techskillTypeList.map(techSkillType => {
                     const thisTypeSkills = techSkills.filter((techSkill: TechSkill) => techSkill.type === techSkillType)
                     return (
-                        <div key={techSkillType} className="flex flex-col p-5 mt-4 lg:my-6 w-1/2 md:w-1/3 lg:w-1/6 lg:hover:scale-105 lg:hover:-translate-y-3 lg:hover:rounded-2xl lg:hover:bg-slate-500 lg:hover:bg-opacity-10 duration-300 ">
+                        <div key={techSkillType} className="flex flex-col p-5 mt-4 lg:my-6 w-1/2 md:w-1/3 lg:w-1/6 lg:min-h-[38vh] lg:hover:scale-105 lg:hover:-translate-y-3 lg:hover:rounded-2xl lg:hover:bg-slate-500 lg:hover:bg-opacity-10 duration-300 ">
                             <p className="text-lg lg:text-xl text-green-500 text-left sm:text-left capitalize mb-4 font-bold">{techSkillType}</p>
                             {thisTypeSkills.map(skill => {
                                 const ThisIcon = skillIcons[skill.skillId]
